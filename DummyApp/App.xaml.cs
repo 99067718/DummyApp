1c1f0b47bd5e2c4931e6d14915ffc7d029c22415
Mielesgames ProGamer <emieltlam11@gmail.com>
2022-09-22 09:26:07 +0200

    Add project files.

---
+++ DummyApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace DummyApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ DummyApp/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace DummyApp;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void OnCounterClicked(object sender, EventArgs e)
+	{
+		await DotnetBot.RotateTo(1000, 1000);
+        await Shell.Current.GoToAsync("//Statistics");
+		await DotnetBot.RotateTo(0, 1000);
+	}
+}
